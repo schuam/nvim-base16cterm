d303a06e8d8d25767984e31943823ab681e7c4a1
Andreas Schuster <andreas.schuster@schuam.de>
2024-08-04 19:26:29 +0200

    Fix names of some diff highlight groups
    I wanted all of them in present tense and not a mix.

--- lua/diff.lua
+++ lua/diff.lua
@@ -5,11 +5,11 @@ local make = function(c)
         {hlgroup = 'DiffChange', ctermfg = c.base03, ctermbg = c.base01, cterm = nil},
         {hlgroup = 'DiffDelete', ctermfg = c.base08, ctermbg = c.base01, cterm = nil},
         {hlgroup = 'DiffText', ctermfg = c.base0D, ctermbg = c.base01, cterm = nil},
-        {hlgroup = 'DiffAdded', ctermfg = c.base0B, ctermbg = c.base01, cterm = nil},
+        {hlgroup = 'DiffAdd', ctermfg = c.base0B, ctermbg = c.base01, cterm = nil},
         {hlgroup = 'DiffFile', ctermfg = c.base08, ctermbg = c.base00, cterm = nil},
         {hlgroup = 'DiffNewFile', ctermfg = c.base0B, ctermbg = c.base00, cterm = nil},
         {hlgroup = 'DiffLine', ctermfg = c.base0D, ctermbg = c.base00, cterm = nil},
-        {hlgroup = 'DiffRemoved', ctermfg = c.base08, ctermbg = c.base00, cterm = nil}
+        {hlgroup = 'DiffRemove', ctermfg = c.base08, ctermbg = c.base00, cterm = nil}
     }
     local hlgroup_links = {
     }
